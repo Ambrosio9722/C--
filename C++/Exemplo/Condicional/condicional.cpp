adaf4c918853f5de5bf322f4d1136628bbdab87a
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2023-09-18 17:06:58 -0300

    Add files via upload

---
+++ C++/Exemplo/Condicional/condicional.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int dinheiro = 20;
+
+if(dinheiro == 10)
+  {
+     cout << "você tem a quantia ezata!"<<endl;
+  }
+  else if(dinheiro >= 10)
+  {
+     cout << "seu dinheiro é mais que o suficiente"<< endl;
+  }
+  else
+  {
+     cout << "quantia insuficiente"<< endl;
+  }
+    return 0;
+}
