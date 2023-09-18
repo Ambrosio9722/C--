349f2e1dff4f4949c99d89d92a6d3949e7836575
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2023-09-18 16:49:08 -0300

    Add files via upload

---
+++ C++/Exemplo/Operadores/operadores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   int n1, n2, resultado1,resultado2,resultado3;
+   n1=3;
+   n2=5;
+   resultado1=0;
+   resultado2=0;
+   resultado1 =0;
+   resultado1 = n1+ n2;
+  cout<< "A soma é: "<< resultado1 << endl;
+  resultado2 = n1/ n2;
+  cout<< "A divisão é: "<< resultado1 << endl;
+  resultado1 = n1* n2;
+  cout<< "A multiplicação é: "<< resultado1 << endl;
+    return 0;
+}
