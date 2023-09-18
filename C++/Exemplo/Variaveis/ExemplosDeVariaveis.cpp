d4624395721effb1e8d4deb5a179f8919492c5ad
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2023-09-18 14:42:16 -0300

    Add files via upload

---
+++ C++/Exemplo/Variaveis/ExemplosDeVariaveis.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream> //<-- biblioteca para ler e escrever
+
+using namespace std;
+
+int main()
+{
+    int numeroInteiro;
+    float pi = 3.14;
+    double numeroPreciso = 3.1234;
+    numeroInteiro = 20;
+    cout << numeroInteiro<< endl;
+    cout << pi << endl;
+    return 0;
+}
---
+++ C++/Exemplo/aula_1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //<-- biblioteca para ler e escrever
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
