a454fa07e0574b95118617718b22325e4bd1377f
Ambrosio9722 <123590853+Ambrosio9722@users.noreply.github.com>
2023-09-20 23:29:29 -0300

    Add files via upload

---
+++ C++/Exemplo/Loop/Loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n = 0;
+ // while
+   while(n<=100)
+   {
+      cout << n <<endl;
+      n++;
+   }
+    cout << "----------------------------------------------------------------------------------" << endl;
+   for(int i = 0; i<=100; i++){
+     cout << i << endl;
+   }
+    return 0;
+}
